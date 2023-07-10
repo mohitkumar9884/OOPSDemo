325ac721e5a88c1325af9a9861de8c03d0d3f352
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-10 15:19:17 +0530

    [mohitkumar9884] adding/creating car.cs

---
+++ OOPSDemo/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSDemo
+{
+    internal class Car
+    {
+        //Attributes or Variables
+        public string model;
+        public string colour;
+        public double price;
+
+        public void Drive()
+        {
+            Console.WriteLine("Turn On Ignition");
+            Console.WriteLine("Car Can Drive");
+        }
+        public void CarDetails()
+        {
+            Console.WriteLine("Model:{0} Colour:{1} Price:{2}",model,colour,price);
+        }
+    }
+}
--- OOPSDemo/Program.cs
+++ OOPSDemo/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Car car1 = new Car();
+            car1.model = "Mustang";
+            car1.colour = "Matt Black";
+            car1.price = 7500000;
+            car1.CarDetails();
+
+            Car car2 = new Car();
+            car2.model = "Merc";
+            car2.colour = "Jupiter Red";
+            car2.price = 7000000;
+            car2.CarDetails();
+
         }
     }
 }
